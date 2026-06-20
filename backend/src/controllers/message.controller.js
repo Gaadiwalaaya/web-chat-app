a30551120315b2cdf338e6de0a3c6128d1198a8e
Gaadiwalaaya <akshathupreti@gmail.com>
2026-06-20 13:22:42 +0530

    websocket left only

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -31,4 +31,32 @@ export const getMessages = async (req, res) => {
     }
 }
 
-export const 
+export const sendMessage = async (req,res) => {
+    try{
+        const {text, image } = req.body;
+        const { id : receiverId } = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image : imageUrl
+
+        });
+
+        await newMessage.save();
+
+        // add socket io
+
+        res.status(200).json({ message : "Message sent"});
+    } catch (error) {
+        res.status(400).json({ "system error " : error.message});
+    }
+}
